1a7936f5881b2160f5559cb64896e773d7724b61
Brody <minibt93@gmail.com>
2021-11-27 10:34:09 -0500

    Add myscript.js

---
+++ myscript.js
@@ -0,0 +1,44 @@
+function computerPlay() {
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0){
+        return "rock";
+    }
+    else if (num === 1){
+        return "paper";
+    }
+    else {
+        return "scissors"
+    }
+}
+
+function playRound(playerChoice, computerChoice){
+    let newPlayerChoice = playerChoice.toLowerCase();
+    if (newPlayerChoice === computerChoice) {
+        return "It is a tie"
+    }
+    else if (newPlayerChoice === "rock"){
+        if (computerChoice === "scissors") {
+            return "You Win"
+        }
+        else{
+            return "You Lose"
+        }
+    }
+    else if (newPlayerChoice === "paper"){
+        if (computerChoice === "rock") {
+            return "You Win"
+        }
+        else{
+            return "You Lose"
+        }
+    }
+    else if (newPlayerChoice === "scissors"){
+        if (computerChoice === "paper") {
+            return "You Win"
+        }
+        else{
+            return "You Lose"
+        }
+    }
+}
+
