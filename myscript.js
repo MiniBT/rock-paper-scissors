fd97131d8bf21698a3e56d252a4118e916d6288b
Brody <minibt93@gmail.com>
2021-12-10 11:14:29 -0500

    Changed game to work with gui instead of typing

--- myscript.js
+++ myscript.js
@@ -12,42 +12,73 @@ function computerPlay() {
 }
 
 function playRound(playerChoice, computerChoice){
-    let newPlayerChoice = playerChoice.toLowerCase();
-    if (newPlayerChoice === computerChoice) {
-        return "It is a tie"
+    if (playerChoice === computerChoice) {
+        updateTie();
     }
-    else if (newPlayerChoice === "rock"){
-        if (computerChoice === "scissors") {
-            return "You Win"
-        }
-        else{
-            return "You Lose"
-        }
+    else if (playerChoice === "rock" && computerChoice === "scissors") {
+        updateWin();
     }
-    else if (newPlayerChoice === "paper"){
-        if (computerChoice === "rock") {
-            return "You Win"
-        }
-        else{
-            return "You Lose"
-        }
+    else if (playerChoice === "paper" && computerChoice === "rock") {
+        updateWin();
     }
-    else if (newPlayerChoice === "scissors"){
-        if (computerChoice === "paper") {
-            return "You Win"
-        }
-        else{
-            return "You Lose"
-        }
+    else if (playerChoice === "scissors" && computerChoice === "paper") {
+        updateWin();
     }
+    else {
+        updateLose();
+    }
+    if (playerScore == 5 || computerScore == 5){
+        endGame(playerScore, computerScore);
+    }
+
 }
 
-function game (){
-    for (let i = 0; i < 5; i++){
-        let playerSelection = prompt("rock, paper, or scissors?");
+function updateWin(){
+    playerScore++
+    results.innerHTML = `You Win! <br><br>
+    Player: ${playerScore} <br>
+    Computer: ${computerScore}`;
+}
+
+function updateLose(){
+    computerScore++
+    results.innerHTML = `You Lose! <br><br>
+    Player: ${playerScore} <br>
+    Computer: ${computerScore}`;
+}
+
+function updateTie(){
+    results.innerHTML = `You Tied! <br><br>
+    Player: ${playerScore} <br>
+    Computer: ${computerScore}`;
+}
+
+function endGame(playerScore, computerScore){
+    const div = document.createElement('div');
+    if (playerScore == 5) {
+        div.textContent = "Congrats, you Win. Please refresh to play again"
+    }
+    else {
+        div.textContent = "Sorry, you lost. Please refresh to try again"
+    }
+    body.appendChild(div);
+    buttons.forEach((button) => {
+        button.disabled = true;
+    })
+}
+
+const results = document.querySelector('#result');
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.value;
         let computerChoice = computerPlay();
         console.log(playRound(playerSelection, computerChoice));
-    }
-}
-
-game()
+    })
+})
